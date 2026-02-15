0ef93dab74e6376cae1fda506f7729fb3055947e
Odai Alghamdi <aalghamdi@glance.care>
2026-02-15 18:26:25 +0300

    logo and other stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@
 # Run your initialization scripts
 python test_scripts_for_admin_dashboard/add_cities.py
 python test_scripts_for_admin_dashboard/create_users.py
-python test_scripts_for_admin_dashboard/check_and_create_admin.py
+python test_scripts_for_admin_dashboard/check_and_create_admin.
+python test_scripts_for_admin_dashboard/add_reviews_table.py
 
 # Start the FastAPI app
-exec uvicorn main:app --host 0.0.0.0 --port 3000
+exec uvicorn main:app --host 0.0.0.0 --port 3000 --proxy-headers --forwarded-allow-ips="*"
