8c8441d960e43551bb99a51a394088c5cf5deb3c
Odai Alghamdi <aalghamdi@glance.care>
2026-02-16 01:21:55 +0300

    fix admin

--- backend/admin.py
+++ backend/admin.py
@@ -12,7 +12,7 @@ from datetime import date
 class UserAdmin(ModelView, model=User):
     column_list = [User.id, User.phone_number, User.email, User.name, User.date_of_birth, User.is_verified, User.otp, User.otp_created_at, User.is_admin, User.role, User.admin_username, User.admin_password_hash, User.city]
     column_searchable_list = [User.phone_number, User.email, User.name]
-    column_filters = [User.is_verified, User.role, User.is_admin]
+    #column_filters = [User.is_verified, User.role, User.is_admin]
 
     column_choices = {
         User.role: {
@@ -36,12 +36,12 @@ class CityAdmin(ModelView, model=City):
     __name__ = "Cities"
     column_list = [City.id, City.name, City.icon, City.active]
     column_searchable_list = [City.name]
-    column_filters = [City.active]
+    #column_filters = [City.active]
 
 class OrderAdmin(ModelView, model=Order):
     column_list = [Order.id, Order.order_id, Order.created_by_user, Order.assigned_to_user, Order.description, Order.creation_date, Order.delivery_date, Order.status, Order.comments, Order.updated_at, Order.city]
     column_searchable_list = [Order.order_id, Order.description]
-    column_filters = [Order.status, Order.city]
+    #column_filters = [Order.status, Order.city]
     form_excluded_columns = [Order.creation_date, Order.updated_at]
 
     column_choices = {
@@ -59,7 +59,7 @@ class OrderAdmin(ModelView, model=Order):
 class InvoiceAdmin(ModelView, model=Invoice):
     column_list = [Invoice.id, Invoice.invoice_id, Invoice.order, Invoice.full_amount, Invoice.service_fee, Invoice.order_only_price, Invoice.courier_fee, Invoice.status, Invoice.description, Invoice.comment, Invoice.sent_to_user_via_email, Invoice.sent_at, Invoice.due_date, Invoice.tax_amount, Invoice.discount_amount, Invoice.promocode, Invoice.created_at, Invoice.updated_at]
     column_searchable_list = [Invoice.invoice_id]
-    #column_filters = [Invoice.status, Invoice.sent_to_user_via_email]
+    ##column_filters = [Invoice.status, Invoice.sent_to_user_via_email]
     form_excluded_columns = [Invoice.created_at, Invoice.updated_at]
 
     column_choices = {
@@ -75,7 +75,7 @@ class InvoiceAdmin(ModelView, model=Invoice):
 class ConversationAdmin(ModelView, model=Conversation):
     column_list = [Conversation.id, Conversation.customer, Conversation.courier, Conversation.status, Conversation.created_at]
     column_searchable_list = [Conversation.customer, Conversation.courier]
-    column_filters = [Conversation.status]
+    #column_filters = [Conversation.status]
     form_excluded_columns = [Conversation.created_at]
 
     column_choices = {
@@ -89,7 +89,7 @@ class ConversationAdmin(ModelView, model=Conversation):
 class MessageAdmin(ModelView, model=Message):
     column_list = [Message.id, Message.conversation, Message.sender, Message.content, Message.sent_at, Message.message_type]
     column_searchable_list = [Message.content]
-    column_filters = [Message.message_type, Message.conversation, Message.sender]
+    #column_filters = [Message.message_type, Message.conversation, Message.sender]
     form_excluded_columns = [Message.sent_at]
 
     column_choices = {
@@ -102,13 +102,13 @@ class MessageAdmin(ModelView, model=Message):
 class WalletAdmin(ModelView, model=Wallet):
     column_list = [Wallet.id, Wallet.user, Wallet.balance, Wallet.created_at, Wallet.updated_at]
     column_searchable_list = [Wallet.user]
-    column_filters = [Wallet.user]
+    #column_filters = [Wallet.user]
     form_excluded_columns = [Wallet.created_at, Wallet.updated_at]
 
 class PaymentAdmin(ModelView, model=Payment):
     column_list = [Payment.id, Payment.invoice, Payment.user, Payment.amount, Payment.payment_method, Payment.status, Payment.transaction_id, Payment.payment_date, Payment.created_at, Payment.updated_at]
     column_searchable_list = [Payment.transaction_id]
-    column_filters = [Payment.status, Payment.payment_method, Payment.user, Payment.invoice]
+    #column_filters = [Payment.status, Payment.payment_method, Payment.user, Payment.invoice]
     form_excluded_columns = [Payment.created_at, Payment.updated_at]
 
     column_choices = {
@@ -129,7 +129,7 @@ class PaymentAdmin(ModelView, model=Payment):
 class PromocodeAdmin(ModelView, model=Promocode):
     column_list = [Promocode.id, Promocode.name, Promocode.code, Promocode.percentage, Promocode.max_value, Promocode.minimum_order_value, Promocode.usage_limit, Promocode.usage_count, Promocode.valid_until, Promocode.active, Promocode.applicable_to, Promocode.created_at]
     column_searchable_list = [Promocode.name, Promocode.code]
-    column_filters = [Promocode.active, Promocode.applicable_to]
+    #column_filters = [Promocode.active, Promocode.applicable_to]
     form_excluded_columns = [Promocode.created_at, Promocode.updated_at]
 
     column_choices = {
@@ -143,7 +143,7 @@ class PromocodeAdmin(ModelView, model=Promocode):
 class ReviewAdmin(ModelView, model=Review):
     column_list = [Review.id, Review.reviewer, Review.reviewed_user, Review.rate, Review.comment, Review.created_at]
     column_searchable_list = [Review.comment]
-    column_filters = [Review.rate, Review.reviewer, Review.reviewed_user]
+    #column_filters = [Review.rate, Review.reviewer, Review.reviewed_user]
     form_excluded_columns = [Review.created_at]
 
     column_choices = {
